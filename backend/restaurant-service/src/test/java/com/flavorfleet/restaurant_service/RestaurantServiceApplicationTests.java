292184418097cdc159e449b8cc510b9c767f8ad5
Aryan Shriva <aryanshriva08@gmail.com>
2025-09-24 00:07:21 +0530

    Day 9: updated pom

--- backend/restaurant-service/src/test/java/com/flavorfleet/restaurant_service/RestaurantServiceApplicationTests.java
+++ backend/restaurant-service/src/test/java/com/flavorfleet/restaurant_service/RestaurantServiceApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestaurantServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class RestaurantServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
